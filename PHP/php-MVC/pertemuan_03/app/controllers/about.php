72ae85c4058765d9d3fdbb90475b3c254d894ac2
AN-maz <andrianian398@gmail.com>
2025-08-28 09:07:57 +0700

    menambahkan folder pertemuan 2 dan 3

---
+++ PHP/php-MVC/pertemuan_03/app/controllers/about.php
@@ -0,0 +1,13 @@
+<?php 
+
+class About{
+    
+    public function index($name = 'Muslim',$pekerjaan = 'tergantung'){
+
+        echo "Halo! nama saya $name, sy seorang $pekerjaan Lho!";
+    }
+    public function page(){
+
+        echo 'About/page';
+    }
+}
