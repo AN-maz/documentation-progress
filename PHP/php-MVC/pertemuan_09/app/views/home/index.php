6522cd338c5a8b0d688de560fb16bf863013aa82
AN-maz <andrianian398@gmail.com>
2025-08-29 21:11:53 +0700

    Menambahkan folder pertemuan 09

---
+++ PHP/php-MVC/pertemuan_09/app/views/home/index.php
@@ -0,0 +1,12 @@
+<!-- <div class="container"> -->
+    <div class="p-5 m-5 bg-light rounded-3">
+        <div class="container-fluid py-5">
+            <h1 class="display-5 fw-bold">Selamat Datang di website saya!</h1>
+            <p class="col-md-8 fs-4">  
+                Halo! Nama sy <?= $data['nama']; ?> 
+            </p>
+            <button class="btn btn-primary btn-lg" type="button">Learn more</button>
+        </div>
+    </div>
+
+<!-- </div> -->
---
+++ PHP/php-MVC/pertemuan_09/readMe.md
@@ -0,0 +1,5 @@
+MVC - Flash Message
+Digunakan setelah menjalankan aksi di dalam CRUD 
+Untuk membuat Flash Message ini membutuhkan $_SESSION 
+
+
