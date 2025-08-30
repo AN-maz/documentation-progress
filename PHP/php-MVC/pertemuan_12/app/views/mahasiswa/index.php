8f06152ff5ef1024ae290d3e199846c1ffd8ae24
AN-maz <andrianian398@gmail.com>
2025-08-30 17:41:57 +0700

    penambahkan pertemuan 12

---
+++ PHP/php-MVC/pertemuan_12/app/views/mahasiswa/index.php
@@ -0,0 +1,110 @@
+<div class="container mt-4">
+
+    <div class="row">
+        <div class="col-lg-6">
+            <?php Flasher::flash(); ?>
+        </div>
+    </div>
+
+    <div class="row mb-3">
+        <div class="col-lg-6">
+            <button type="button" class="btn btn-primary tombolTambahData" data-bs-toggle="modal" data-bs-target="#formModal">
+                Tambah Data Mahasiswa
+            </button>
+        </div>
+    </div>
+
+    <div class="row">
+        <div class="col-lg-6">
+            <form action="<?= BASEURL; ?>/mahasiswa/cari" method="post">
+
+                <div class="input-group mb-3">
+                    <input type="text" class="form-control" placeholder="Cari Mahasiswa..." name="keyword" id="keyword" autocomplete="off">
+                    <button class="btn btn-primary" type="submit" id="TombolCari">Cari</button>
+                </div>
+
+            </form>
+        </div>
+    </div>
+
+    <div class="row">
+        <div class="col-lg-6">
+
+            <h3>Daftar Mahasiswa</h3>
+
+            <ul class="list-group">
+                <?php foreach ($data['mhs'] as $mhs) : ?>
+                    <li class="list-group-item d-flex justify-content-between align-items-center">
+                        <span><?= $mhs['nama']; ?></span>
+                        <div>
+                            <a href="<?= BASEURL; ?>/mahasiswa/detail/<?= $mhs['id']; ?>" class="badge text-bg-info me-1">Detail</a>
+
+                            <a href="<?= BASEURL; ?>/mahasiswa/ubah/<?= $mhs['id']; ?>" class="badge text-bg-success tombolModalUbah" data-bs-toggle="modal" data-bs-target="#formModal" data-id="<?= $mhs['id']; ?>">Ubah</a>
+
+                            <a href="<?= BASEURL; ?>/mahasiswa/hapus/<?= $mhs['id']; ?>" class="badge text-bg-danger" onclick="return confirm('Yakin MasPur!?')">Hapus</a>
+                        </div>
+                    </li>
+                <?php endforeach ?>
+            </ul>
+
+        </div>
+    </div>
+
+</div>
+
+<!-- Modal -->
+<div class="modal fade" id="formModal" tabindex="-1" aria-labelledby="judulModalLabel" aria-hidden="true">
+    <div class="modal-dialog">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h1 class="modal-title fs-5" id="judulModalLabel">Tambah Data Mahasiswa</h1>
+                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+
+            <div class="modal-body">
+
+                <form action="<?= BASEURL; ?>/mahasiswa/tambah" method="post">
+
+                    <input type="hidden" name="id" id="id">
+
+                    <div class="mb-3">
+                        <label for="nama" class="form-label">Nama</label>
+                        <input type="text" class="form-control" id="nama" name="nama">
+                    </div>
+
+                    <div class="mb-3">
+                        <label for="nim" class="form-label">NIM</label>
+                        <input type="number" class="form-control" id="nim" name="nim">
+                    </div>
+
+                    <div class="mb-3">
+                        <label for="email" class="form-label">Email</label>
+                        <input type="email" class="form-control" id="email" name="email">
+                    </div>
+
+                    <div class="mb-3">
+                        <label for="jurusan" class="form-label">Jurusan</label>
+                        <select class="form-select" id="jurusan" name="jurusan">
+                            <!-- <option selected disabled>-- Pilih salah satu --</option> -->
+                            <option value="Teknik Informatika">Teknik Informatika</option>
+                            <option value="Teknik Industri">Teknik Industri</option>
+                            <option value="Teknik Mesin">Teknik Mesin</option>
+                            <option value="Sistem Informasi">Sistem Informasi</option>
+                            <option value="Teknik Komputer">Teknik Komputer</option>
+                            <option value="Teknik Elektro">Teknik Elektro</option>
+                            <option value="Manajemen Informatika">Manajemen Informatika</option>
+                        </select>
+                    </div>
+
+
+                    <div class="modal-footer">
+                        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                        <button type="submit" class="btn btn-primary">Tambah Data</button>
+                    </div>
+
+                </form>
+            </div>
+
+        </div>
+    </div>
+</div>
---
+++ PHP/php-MVC/pertemuan_12/readMe.md
@@ -0,0 +1,3 @@
+MVC - Search Data
+
+
