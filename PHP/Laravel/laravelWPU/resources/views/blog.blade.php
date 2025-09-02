be98a651dd4efefab3f31a7543eec583607a78f5
AN-maz <andrianian398@gmail.com>
2025-09-02 08:21:37 +0700

    menambahkan folder laravelWPU

---
+++ PHP/Laravel/laravelWPU/resources/views/blog.blade.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Halaman Blog</title>
+</head>
+
+<body>
+    <a href="/">Home</a>
+    <a href="/blog">Blog</a>
+    <a href="/about">About</a>
+    <a href="/contact">Contact</a>  
+
+    <h1>Halaman Blog</h1>
+
+    <article>
+        <h3>Judul 1</h3>
+        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Omnis id recusandae necessitatibus quam, vitae
+            facilis commodi eos dolor repellat reprehenderit quaerat libero voluptatum culpa, assumenda laboriosam
+            suscipit quasi eligendi reiciendis.</p>
+    </article>
+
+    <article>
+        <h3>Judul 2</h3>
+        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Omnis id recusandae necessitatibus quam, vitae
+            facilis commodi eos dolor repellat reprehenderit quaerat libero voluptatum culpa, assumenda laboriosam
+            suscipit quasi eligendi reiciendis.</p>
+    </article>
+</body>
+
+</html>
