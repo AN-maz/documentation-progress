b9e59386d1aa86c542eba37f87a795b970de12a5
AN-maz <andrianian398@gmail.com>
2025-09-02 10:01:00 +0700

    penyesuaian folder

--- PHP/Laravel/laravel11/resources/views/components/page-header.blade.php
+++
@@ -1,8 +0,0 @@
-@props(['title', 'subtitle' => ''])
-
-<div class="page-header">
-    <h1>{{ $title }}</h1> {{-- <--- PERBAIKI SEPERTI INI --}}
-    @if($subtitle)
-        <p class="subtitle">{{ $subtitle }}</p>
-    @endif
-</div>
