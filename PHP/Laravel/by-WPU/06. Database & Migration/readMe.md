5bf0137608979aa1b9243734c9e05f04febd4391
AN-maz <andrianian398@gmail.com>
2025-09-03 14:47:14 +0700

    menambahkan folder 06. database dan migrate

---
+++ PHP/Laravel/by-WPU/06. Database & Migration/readMe.md
@@ -0,0 +1,10 @@
+___Database & Migration__
+
+Menggunakan `tablePlus` untuk mengelola database 
+
+dalam folder ini hanya mempersiapkan database saja dan menambahkan 
+file : 2025_09_03_071904_create_posts_table
+untuk schema databasenya. Dan insert table di tablePlus
+
+Yang baru:
+- folder database
---
+++ PHP/Laravel/by-WPU/06. Database & Migration/routes/web.php
@@ -0,0 +1,38 @@
+<?php
+
+use App\Models\Post;
+use Illuminate\Support\Arr;
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+    return view('home', ['title' => 'Home Page']);
+});
+
+
+Route::get('/about', function () {
+    return view('about', [
+        'nama' => 'Purwa Guri',
+        'title' => 'About Page'
+    ]);
+});
+
+
+// VIEW DATA
+Route::get('/blog', function () {
+    return view('blog',['title' => 'Blog', 'posts' => Post::all()]);
+});
+
+// VIEW -> mengirimkan data ke halaman SIngle post
+Route::get('/blog/{slug}', function ($slug) {
+
+    $post = Post::find($slug);
+
+    return view('post', ['title' => 'Single Post', 'post' => $post]);
+});
+
+
+Route::get('/contact', function () {
+    return view('contact', ['title' => 'Contact Page']);
+});
+
+
