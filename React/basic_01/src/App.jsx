4674e2272b9223bed837596776af8f08c33e9366
AN-maz <andrianian398@gmail.com>
2025-09-25 13:40:34 +0700

    menerapkan giliran pemain (x dan O)

--- React/basic_01/src/App.jsx
+++ React/basic_01/src/App.jsx
@@ -1,29 +1,45 @@
 import { useState } from 'react'
 
-function Square() {
-  const [value, setvalue] = useState('');
+function Square({ value, onSquareClick }) {
 
-  function handleClick() {
-    setvalue('x');
-  }
-  
-  return <button className="square" onClick={handleClick}>
+  return <button className="square" onClick={onSquareClick}>
     {value}
   </button>;
 }
 
 export default function Board() {
+
+  const [squares, setSquares] = useState(Array(9).fill(null));
+  const [xIsNext, setXisNext] = useState(true);
+
+  function handleClick(i) {
+
+    if (squares[i]) {
+      return;
+    }
+    const nextSquare = squares.slice();
+
+    if (xIsNext) {
+      nextSquare[i] = 'X';
+    } else {
+      nextSquare[i] = 'O';
+    }
+    setSquares(nextSquare);
+    setXisNext(!xIsNext)
+  }
+
   return (
     <div className="board">
-      <Square />
-      <Square />
-      <Square />
-      <Square />
-      <Square />
-      <Square />
-      <Square />
-      <Square />
-      <Square />
+      <Square value={squares[0]} onSquareClick={() => handleClick(0)} />
+      <Square value={squares[1]} onSquareClick={() => handleClick(1)} />
+      <Square value={squares[2]} onSquareClick={() => handleClick(2)} />
+      <Square value={squares[3]} onSquareClick={() => handleClick(3)} />
+      <Square value={squares[4]} onSquareClick={() => handleClick(4)} />
+      <Square value={squares[5]} onSquareClick={() => handleClick(5)} />
+      <Square value={squares[6]} onSquareClick={() => handleClick(6)} />
+      <Square value={squares[7]} onSquareClick={() => handleClick(7)} />
+      <Square value={squares[8]} onSquareClick={() => handleClick(8)} />
     </div>
   )
+
 }
