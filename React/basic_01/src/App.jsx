dd9256b7e75538a3223cc45cce10aafc1734342d
AN-maz <andrianian398@gmail.com>
2025-09-25 15:53:40 +0700

    final game dan desain

--- React/basic_01/src/App.jsx
+++ React/basic_01/src/App.jsx
@@ -1,10 +1,11 @@
-import { useState } from 'react'
+import { useState } from 'react';
 
 function Square({ value, onSquareClick }) {
-
-  return <button className="square" onClick={onSquareClick}>
-    {value}
-  </button>;
+  return (
+    <button className="square" onClick={onSquareClick}>
+      {value}
+    </button>
+  );
 }
 
 function calculateWinner(squares) {
@@ -12,13 +13,13 @@ function calculateWinner(squares) {
     [0, 1, 2],
     [3, 4, 5],
     [6, 7, 8],
+    [0, 3, 6],
     [1, 4, 7],
     [2, 5, 8],
     [0, 4, 8],
-    [2, 4, 6]
+    [2, 4, 6],
   ];
-  for (let i = 0; i < lines.length; i++) {
-    const [a, b, c] = lines[i];
+  for (let [a, b, c] of lines) {
     if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
       return squares[a];
     }
@@ -26,52 +27,69 @@ function calculateWinner(squares) {
   return null;
 }
 
-export default function Board() {
-
-  const [squares, setSquares] = useState(Array(9).fill(null));
-  const [xIsNext, setXisNext] = useState(true);
-
+function Board({ xIsNext, squares, onPlay }) {
   function handleClick(i) {
-
-    const winner = calculateWinner(squares);
-    if (winner || squares[i]) {
-      return;
-    }
-    const nextSquare = squares.slice();
-    if (xIsNext) {
-      nextSquare[i] = 'X';
-    } else {
-      nextSquare[i] = 'O';
-    }
-
-    setSquares(nextSquare);
-    setXisNext(!xIsNext);
+    if (calculateWinner(squares) || squares[i]) return;
+    const nextSquares = squares.slice();
+    nextSquares[i] = xIsNext ? 'X' : 'O';
+    onPlay(nextSquares);
   }
 
   const winner = calculateWinner(squares);
-  let status;
-  if (winner) {
-    status = 'Pemenang: ' + winner;
-  } else {
-    status = 'Giliran Berikutnya: ' + (xIsNext ? 'X' : 'O');
-  }
+  const status = winner
+    ? 'Pemenang: ' + winner
+    : 'Giliran Berikutnya: ' + (xIsNext ? 'X' : 'O');
 
   return (
     <>
-      <div className="status"> {status} </div>
+      <div className="status">{status}</div>
       <div className="board">
-        <Square value={squares[0]} onSquareClick={() => handleClick(0)} />
-        <Square value={squares[1]} onSquareClick={() => handleClick(1)} />
-        <Square value={squares[2]} onSquareClick={() => handleClick(2)} />
-        <Square value={squares[3]} onSquareClick={() => handleClick(3)} />
-        <Square value={squares[4]} onSquareClick={() => handleClick(4)} />
-        <Square value={squares[5]} onSquareClick={() => handleClick(5)} />
-        <Square value={squares[6]} onSquareClick={() => handleClick(6)} />
-        <Square value={squares[7]} onSquareClick={() => handleClick(7)} />
-        <Square value={squares[8]} onSquareClick={() => handleClick(8)} />
+        {squares.map((val, i) => (
+          <Square key={i} value={val} onSquareClick={() => handleClick(i)} />
+        ))}
       </div>
     </>
-  )
-
+  );
 }
 
+export default function Game() {
+  const [history, setHistory] = useState([Array(9).fill(null)]);
+  const [currentMove, setCurrentMove] = useState(0);
+  const xIsNext = currentMove % 2 === 0;
+  const currentSquares = history[currentMove];
+
+  function handlePlay(nextSquares) {
+    const nextHistory = [...history.slice(0, currentMove + 1), nextSquares];
+    setHistory(nextHistory);
+    setCurrentMove(nextHistory.length - 1);
+  }
+
+  function jumpTo(nextMove) {
+    setCurrentMove(nextMove);
+  }
+
+  const moves = history.map((squares, move) => {
+    const description =
+      move > 0 ? 'Pergi ke langkah #' + move : 'Pergi ke awal game';
+    return (
+      <li key={move}>
+        <button onClick={() => jumpTo(move)}>{description}</button>
+      </li>
+    );
+  });
+
+  return (
+    <div className="game">
+      <div className="game-board">
+        <Board
+          xIsNext={xIsNext}
+          squares={currentSquares}
+          onPlay={handlePlay}
+        />
+      </div>
+      <div className="game-info">
+        <ol>{moves}</ol>
+      </div>
+    </div>
+  );
+}
