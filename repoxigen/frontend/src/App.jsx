3aae79e71339a7492683eb8d6fadb72984d2609c
Andrian Maulana <andrianian398@gmail.com>
2026-02-10 14:07:41 +0700

    feat:install-tailwind-router-dom-in-fronted

--- repoxigen/frontend/src/App.jsx
+++ repoxigen/frontend/src/App.jsx
@@ -24,11 +24,11 @@ function App() {
   }, [])
 
   return (
-    <div className="">
-      <h1>Hallo MasPur!</h1>
+    <div className="min-h-screen bg-gray-100 flex flex-col items-center justify-center p-4">
+      <h1 className='text-3xl font-bold text-blue-600 mb-4'>Hallo MasPur!</h1>
 
-      <div className="">
-        <h2>Status Koneksi backend:</h2>
+      <div className="bg-white p-6 rounded-xl shadow-md w-full max-w-md text-center">
+        <h2 className='text-xl font-semibold mb-2'>Status Koneksi backend:</h2>
 
         {loading && <p>Sedang memuat data...</p>}
 
@@ -46,7 +46,7 @@ function App() {
         )}
       </div>
 
-      <button onClick={() => window.location.reload()}>
+      <button onClick={() => window.location.reload()} className='mt-6 bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600 transition'>
         Refresh Data
       </button>
     </div>
