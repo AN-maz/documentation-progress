c9ce1b486bdae7f445d0d6ee8a6c895308c82d41
Andrian Maulana <andrianian398@gmail.com>
2026-03-18 16:54:17 +0700

    feat:move-project-web

--- repoxigen/backend/prisma/migrations/20260217232929_revisi_table_users/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - You are about to drop the column `deskripsi` on the `divisi` table. All the data in the column will be lost.
-
-*/
--- AlterTable
-ALTER TABLE `divisi` DROP COLUMN `deskripsi`;
--- repoxigen/backend/routes/userRoutes.js
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-import {getMyProfile} from '../controllers/userController.js';
-import { verifyToken } from '../middlewares/authMiddleware.js';
-
-const router = express.Router();
-
-router.get('/me', verifyToken,getMyProfile);
-
-export default router;
