1c34cfa4ff58ff5c3c8e72d7520cfee36dc6ab03
Andrian Maulana <andrianian398@gmail.com>
2026-03-22 21:57:31 +0700

    first commit

---
+++ JavaScript/test1/script.js
@@ -0,0 +1,87 @@
+// Hello World
+document.writeln("Hello world");
+document.writeln("<h1>Hello World (h1)</h1>");
+document.writeln("<hr>");
+
+// =======================
+// Tipe Data
+// =======================
+
+// Number (angka)
+document.writeln(100);
+document.writeln("<br>");
+document.writeln(100.101);
+
+// Number Notation
+document.writeln("<br>");
+
+// Biner (basis 2)
+document.writeln(0b1111010); // 122
+
+// Hexadecimal (basis 16)
+document.writeln("<br>");
+document.writeln(0x7A); // 122
+
+// Octal (basis 8)
+document.writeln("<br>");
+document.writeln(0o172); // 122
+
+document.writeln("<hr>");
+
+// =======================
+// String
+// =======================
+document.writeln("Hello");
+document.writeln("<br>");
+document.writeln('Belajar JavaScript');
+document.writeln("<br>");
+document.writeln('Saya: ' + 'Seorang ' + ' Programmer');
+
+
+// Escape Character
+document.writeln("<br>");
+document.writeln("Nama: \"ANmaz\"");
+document.writeln("<br>");
+document.writeln('Ini kutip satu: \'JS\'');
+document.writeln("<hr>");
+
+// =======================
+// Boolean
+// =======================
+document.writeln(true);
+document.writeln("<br>");
+document.writeln(false);
+
+document.writeln("<hr>");
+
+// =======================
+// Variable
+// =======================
+let nama = "ANmaz";
+let umur = 20;
+
+document.writeln("Nama: " + nama);
+document.writeln("<br>");
+document.writeln("Umur: " + umur);
+
+document.writeln("<hr>");
+
+const app = "Belajar Js"
+// app = "Ga mau belajar"  [ini error]
+
+
+// =======================
+// Operator Matematika
+// =======================
+let a = 10;
+let b = 5;
+
+document.writeln(a + b); // tambah
+document.writeln("<br>");
+document.writeln(a - b); // kurang
+document.writeln("<br>");
+document.writeln(a * b); // kali
+document.writeln("<br>");
+document.writeln(a / b); // bagi
+document.writeln("<br>");
+document.writeln(a % b); // sisa bagi
