c64d21775c5086e18241b93e5d1af114966124d5
Andrian Maulana <andrianian398@gmail.com>
2026-06-16 17:53:35 +0700

    feat:revisi product.service.js

--- RESTful-express/latihan/src/services/product.service.js
+++ RESTful-express/latihan/src/services/product.service.js
@@ -6,8 +6,7 @@ const productService = {
       SELECT p.id, p.name, p.price, c.id AS category_id, c.name AS category_name
       FROM products p
       INNER JOIN categories c ON p.category_id = c.id
-    `; // Sedikit tips: tambahkan c.id AS category_id agar memudahkan frontend saat mode edit nanti
-
+    `; 
     const [rows] = await db.query(query);
     return rows;
   },
@@ -15,21 +14,19 @@ const productService = {
   createProduct: async (productData) => {
     const { name, price, category_name } = productData;
     
-    // 1. Logika Cek atau Buat Kategori Baru
     let categoryId;
     
-    // Cek apakah kategori sudah ada
     const [existingCat] = await db.query("SELECT id FROM categories WHERE name = ?", [category_name]);
     
     if (existingCat.length > 0) {
       categoryId = existingCat[0].id;
     } else {
-      // Jika belum ada, buat baru
+
       const [newCat] = await db.query("INSERT INTO categories (name) VALUES (?)", [category_name]);
       categoryId = newCat.insertId;
     }
 
-    // 2. Insert Produk Menggunakan categoryId yang Tepat
+
     const query = "INSERT INTO products (name, price, category_id) VALUES (?, ?, ?)";
     const [result] = await db.query(query, [name, price, categoryId]);
     
@@ -39,7 +36,6 @@ const productService = {
   updateProduct: async (id, productData) => {
     const { name, price, category_name } = productData;
 
-    // 1. Logika Cek atau Buat Kategori Baru saat Update
     let categoryId;
     const [existingCat] = await db.query("SELECT id FROM categories WHERE name = ?", [category_name]);
     
@@ -50,7 +46,6 @@ const productService = {
       categoryId = newCat.insertId;
     }
 
-    // 2. Update data produk dengan categoryId yang baru/ditemukan
     const query = "UPDATE products SET name = ?, price = ?, category_id = ? WHERE id = ?";
     const [result] = await db.query(query, [name, price, categoryId, id]);
     
